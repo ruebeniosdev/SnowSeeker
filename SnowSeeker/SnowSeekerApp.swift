989c2c68051db4a1ac22be179ddd6be4446ae4b6
Akankobateng Rueben <akankobatengr@gmail.com>
2022-09-24 23:16:26 +0000

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by RUEBEN on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
